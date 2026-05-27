ae69359be6398786bfeba03332bf408a6b0fa162
ggpradeep <80970765+ggpradeep@users.noreply.github.com>
2026-05-26 19:57:46 -0700

    Add files via upload

---
+++ swap.py
@@ -0,0 +1,7 @@
+a = int(input("Enter your first number to swap with"))
+b = int(input("Enter your second number to swap with"))
+c = int(input("Enter your third number to swap with"))
+print("This is a left swap", c, a, b)
+print("This is a right swap", b, c, a)
+
+#If there is a bug, it was because i wasn't able to test.
